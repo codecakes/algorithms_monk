30aae1789f8a0a9184542e18ca6550079e78a895
codecakes <akurnya@gmail.com>
2018-05-17 15:22:55 +0530

    WIP priority queue using max_heapify

---
+++ sort/priority_queue.py
@@ -0,0 +1,27 @@
+"""
+Max Priority Queue is based on the structure of max heap and can perform following operations:
+
+maximum(Arr) : It returns maximum element from the Arr.
+extract_maximum (Arr) - It removes and return the maximum element from the Arr.
+increase_val (Arr, i , val) - It increases the key of element stored at index i in Arr to new value val. 
+insert_val (Arr, val ) - It inserts the element with value val in Arr.
+"""
+
+from sort.heapsort import max_heapify
+
+class PQ(object):
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def maximum(self):
+        pass
+    
+    def extract_maximum(self):
+        pass
+    
+    def increase_val(self, idx, val):
+        pass
+    
+    def insert_val(self, val):
+        pass
+
