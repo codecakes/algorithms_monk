dcf1e10befe204e54c1e8d6f6a436ab0b47ce6d8
codecakes <akurnya@gmail.com>
2018-05-28 19:00:40 +0530

    ast bracket check done

---
+++ stacks/ast_brackets.py
@@ -0,0 +1,53 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+DCT = {
+    ')': '(',
+    ']': '[',
+    '}': '{'
+}
+
+
+def ast_bracket(expr):
+    stack = []
+    for br in expr:
+        if stack:
+            sym = DCT.get(br, None)
+            if sym and stack[-1] == sym:
+                stack.pop()
+            elif sym == None:
+                stack.append(br)
+            else:
+                return 'NO'
+        else:
+            stack.append(br)
+    return 'YES' if not stack else 'NO'
+
+
+if __name__ == '__main__':
+    # t = int(raw_input())
+
+    expr = output = res = ''
+    try:    
+        with open('test_ast_brackets.txt', 'r') as F:
+            with open('test_output_ast_brackets.txt') as RES:    
+                t = int(F.readline().strip())
+                print t
+                for t_itr in xrange(t):
+                    expr = expression = F.readline().strip()
+                    res = ast_bracket(expression)
+                    output = RES.readline().strip()
+                    # print "expr is %s and output is: %s and result is: %s" %(expr, output, res)
+                    assert res == output
+    except Exception as e:
+        print e
+        print "Error at", expr, res, output
+    finally:
+        print "DONE!"
+        sys.exit()
+
