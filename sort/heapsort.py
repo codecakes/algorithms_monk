624c8cb08732f1ed02a105082d5a399f5e5d870c
codecakes <akurnya@gmail.com>
2018-06-19 01:19:03 +0530

    implement pq

--- sort/heapsort.py
+++ sort/heapsort.py
@@ -2,11 +2,34 @@ def swap(arr, idx1, idx2):
     arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
 
 
+def get_left(idx): return 2*idx + 1
+def get_right(idx): return 2*idx + 2
+def get_parent(idx): return idx/2 if idx%2 else idx/2 - 1
+
+
+# def min_heapify(arr, idx, N):
+#     if idx<=N:
+#         smallest = None
+#         left_idx = get_left(idx)
+#         right_idx = get_right(idx)
+        
+#         if left_idx <= N:
+#             smallest = min(left_idx, idx, key=lambda x: arr[x])
+#         if right_idx <= N:
+#             smallest = min(smallest, right_idx, key=lambda x: arr[x])
+#         if smallest and smallest != idx:
+#             swap(arr, smallest, idx)
+#             min_heapify(arr, smallest, N)
+#         elif smallest > 0 and arr[get_parent(smallest)] > arr[smallest]:
+#             parent_idx = get_parent(smallest)
+#             swap(arr, smallest, parent_idx)
+#             min_heapify(arr, smallest, N)
+
 def max_heapify(arr, idx, N):
     if idx<=N:
         largest = None
-        left_idx = 2*idx + 1
-        right_idx = 2*idx + 2
+        left_idx = get_left(idx)
+        right_idx = get_right(idx)
         
         if left_idx <= N:
             largest = max(left_idx, idx, key=lambda x: arr[x])
@@ -14,6 +37,10 @@ def max_heapify(arr, idx, N):
             largest = max(largest, right_idx, key=lambda x: arr[x])
         if largest and largest != idx:
             swap(arr, largest, idx)
+            max_heapify(arr, largest, N)
+        elif largest > 0 and arr[get_parent(largest)] < arr[largest]:
+            parent_idx = get_parent(largest)
+            swap(arr, largest, parent_idx)
             max_heapify(arr, largest, N)
 
 def build_maxheap(arr, N):
@@ -28,8 +55,8 @@ def heapsort(arr, N):
     arr = build_maxheap(arr, N)
     while N >= 0:
         swap(arr, 0, N)
-        copyarr += [arr.pop()]
         N -= 1
+        copyarr.insert(0, arr.pop())
         max_heapify(arr, 0, N)
     return copyarr
 
@@ -41,5 +68,5 @@ if __name__ == "__main__":
     N2 = len(arr2)-1
     sorted_arr = heapsort(arr, N)
     sorted_arr2 = heapsort(arr2, N2)
-    assert list(reversed(sorted_arr)) == sorted([3, -100, 4, 1, 2, 6])
-    assert list(reversed(sorted_arr2)) == sorted([3, -100, 4, 2])
+    assert sorted_arr == sorted([3, -100, 4, 1, 2, 6])
+    assert sorted_arr2 == sorted([3, -100, 4, 2])
--- sort/priority_queue.py
+++ sort/priority_queue.py
@@ -7,21 +7,41 @@ increase_val (Arr, i , val) - It increases the key of element stored at index i 
 insert_val (Arr, val ) - It inserts the element with value val in Arr.
 """
 
-from sort.heapsort import max_heapify
+from heapsort import get_parent, swap, max_heapify, build_maxheap, heapsort
 
 class PQ(object):
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, arr):
+        self.arr = arr if isinstance(arr, list) else []
+        self.N = len(arr) if isinstance(arr, list) else 0
+    
+    def setup_build(self):
+        build_maxheap(self.arr, self.N-1)
     
     def maximum(self):
-        pass
+        return self.arr[0] if self.arr else None
     
     def extract_maximum(self):
-        pass
+        ln = self.N - 1
+        if not self.arr:
+            return None
+        swap(self.arr, 0, ln)
+        val = self.arr.pop()
+        ln -= 1
+        max_heapify(self.arr, 0, ln)
+        self.N -= 1
+        return val
+
     
     def increase_val(self, idx, val):
-        pass
+        ln = self.N - 1
+        if idx < self.N:
+            self.arr[idx] = val
+            max_heapify(self.arr, idx, ln)
     
     def insert_val(self, val):
-        pass
+        self.arr.append(val)
+        self.N += 1
+        last_idx = self.N - 1
+        max_heapify(self.arr, get_parent(last_idx), last_idx)
+
 
